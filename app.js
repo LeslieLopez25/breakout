899c5983ca7c9a6a126730fee3e6b82bd2a8643b
Leslie Lopez <leslie2515lopez@gmail.com>
2022-07-22 22:00:20 -0500

    Added function to create blocks

---
+++ app.js
@@ -0,0 +1,29 @@
+const grid = document.querySelector(".grid");
+const blockWidth = 100;
+const blockHeight = 20;
+
+// CREATE BLOCK
+class Block {
+  constructor(xAxis, yAxis) {
+    this.bottomLeft = [xAxis, yAxis];
+    this.bottomRight = [xAxis + blockWidth, yAxis];
+    this.topLeft = [xAxis, yAxis + blockHeight];
+    this.topRight = [xAxis + blockWidth, yAxis + blockHeight];
+  }
+}
+
+// ALL MY BLOCKS
+const blocks = [new Block(10, 270)];
+
+// DRAW ALL MY BLOCKS
+function addBlocks() {
+  for (let i = 0; i < blocks.length; i++) {
+    const block = document.createElement("div");
+    block.classList.add("block");
+    block.style.left = blocks[i].bottomLeft[0] + "px";
+    block.style.bottom = blocks[i].bottomLeft[1] + "px";
+    grid.appendChild(block);
+  }
+}
+
+addBlocks();
