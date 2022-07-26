dabe85a361584e5315028cf7c7e8e1e26ead7dd6
Leslie Lopez <leslie2515lopez@gmail.com>
2022-07-25 20:55:55 -0500

    Added user function

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const grid = document.querySelector(".grid");
 const blockWidth = 100;
 const blockHeight = 20;
 
+const userStart = [230, 10];
+let currentPosition = userStart;
+
 // CREATE BLOCK
 class Block {
   constructor(xAxis, yAxis) {
@@ -13,7 +16,23 @@ class Block {
 }
 
 // ALL MY BLOCKS
-const blocks = [new Block(10, 270)];
+const blocks = [
+  new Block(10, 270),
+  new Block(120, 270),
+  new Block(230, 270),
+  new Block(340, 270),
+  new Block(450, 270),
+  new Block(10, 240),
+  new Block(120, 240),
+  new Block(230, 240),
+  new Block(340, 240),
+  new Block(450, 240),
+  new Block(10, 210),
+  new Block(120, 210),
+  new Block(230, 210),
+  new Block(340, 210),
+  new Block(450, 210),
+];
 
 // DRAW ALL MY BLOCKS
 function addBlocks() {
@@ -25,5 +44,11 @@ function addBlocks() {
     grid.appendChild(block);
   }
 }
-
 addBlocks();
+
+// ADD USER
+const user = document.createElement("div");
+user.classList.add("user");
+user.style.left = currentPosition[0] + "px";
+user.style.bottom = currentPosition[1] + "px";
+grid.appendChild(user);
