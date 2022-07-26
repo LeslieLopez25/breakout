7a813a1c16bf11f5ea6d78cb4bc80e236385bf7d
Leslie Lopez <leslie2515lopez@gmail.com>
2022-07-25 21:39:11 -0500

    Added move ball and game over function

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const grid = document.querySelector(".grid");
+const scoreDisplay = document.querySelector("#score");
 const blockWidth = 100;
 const blockHeight = 20;
+const ballDiameter = 20;
 const boardWidth = 560;
+const boardHeight = 300;
+let timerId;
+let xDirection = 2;
+let yDirection = 2;
 
 const userStart = [230, 10];
 let currentPosition = userStart;
@@ -93,3 +99,51 @@ const ball = document.createElement("div");
 ball.classList.add("ball");
 drawBall();
 grid.appendChild(ball);
+
+// MOVE BALL
+function moveBall() {
+  ballCurrentPosition[0] += xDirection;
+  ballCurrentPosition[1] += yDirection;
+  drawBall();
+  checkForCollisions();
+}
+
+timerId = setInterval(moveBall, 30);
+
+// CHECK FOR COLLISIONS
+function checkForCollisions() {
+  // CHECK FOR WALL COLLISIONS
+  if (
+    ballCurrentPosition[0] >= boardWidth - ballDiameter ||
+    ballCurrentPosition[1] >= boardHeight - ballDiameter ||
+    ballCurrentPosition[0] <= 0
+  ) {
+    changeDirection();
+  }
+
+  // CHECK FOR GAME OVER
+  if (ballCurrentPosition[1] <= 0) {
+    clearInterval(timerId);
+    scoreDisplay.innerHTML = "You Lose";
+    document.removeEventListener("keydown", moveUser);
+  }
+}
+
+function changeDirection() {
+  if (xDirection === 2 && yDirection === 2) {
+    yDirection = -2;
+    return;
+  }
+  if (xDirection === 2 && yDirection === -2) {
+    xDirection = -2;
+    return;
+  }
+  if (xDirection === -2 && yDirection === -2) {
+    yDirection = 2;
+    return;
+  }
+  if (xDirection === -2 && yDirection === 2) {
+    xDirection = 2;
+    return;
+  }
+}
