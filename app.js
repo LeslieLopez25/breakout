3057cbf221645a6c6ac7b43f0f064b047fdd9f6a
Leslie Lopez <leslie2515lopez@gmail.com>
2022-07-26 20:44:23 -0500

    Added block collision function

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const boardHeight = 300;
 let timerId;
 let xDirection = 2;
 let yDirection = 2;
+let score = 0;
 
 const userStart = [230, 10];
 let currentPosition = userStart;
@@ -112,6 +113,22 @@ timerId = setInterval(moveBall, 30);
 
 // CHECK FOR COLLISIONS
 function checkForCollisions() {
+  // CHECK FOR BLOCK COLLISIONS
+  for (let i = 0; i < blocks.length; i++) {
+    if (
+      ballCurrentPosition[0] > blocks[i].bottomLeft[0] &&
+      ballCurrentPosition[0] < blocks[i].bottomRight[0] &&
+      ballCurrentPosition[1] + ballDiameter > blocks[i].bottomLeft[1] &&
+      ballCurrentPosition[1] < blocks[i].topLeft[1]
+    ) {
+      const allBlocks = Array.from(document.querySelectorAll(".block"));
+      allBlocks[i].classList.remove("block");
+      blocks.splice(i, 1);
+      changeDirection();
+      score++;
+      scoreDisplay.innerHTML = score;
+    }
+  }
   // CHECK FOR WALL COLLISIONS
   if (
     ballCurrentPosition[0] >= boardWidth - ballDiameter ||
